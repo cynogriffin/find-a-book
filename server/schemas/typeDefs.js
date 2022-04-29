f8a0b9f5b006523d7a95d47f95411495af0518b8
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-04-29 14:51:46 -0500

    added typeDefs for graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,6 +15,35 @@ const typeDefs = gql`
     type Book {
         bookId: String
         authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    input bookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: bookInput): User
+        removeBook(bookId: String!): User
     }
 `;
 
